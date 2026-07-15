76ec75e6a1a95c8a51221f59cdbf031eead3ac88
MIkhsanB546 <mikhsanb546@gmail.com>
2026-07-15 20:58:20 +0700

    add cuntom font and prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Roboto",
+      cssVariable: "--font-roboto",
     },
+  ],
 });
